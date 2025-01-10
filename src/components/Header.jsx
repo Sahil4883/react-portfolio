f3337f6208138467a5e94d17abb020090bf5a651
Sahil <shaikhsahil4883@gmail.com>
2025-01-10 13:16:23 +0530

    update: Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,11 @@
-import styled from "styled-components";
-
-const HeaderWrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  background-color: #343a40; /* dark background */
-`;
-
-const Title = styled.h1`
-  color: white;
-  font-size: 3rem;
-`;
-
-const Subtitle = styled.p`
-  font-size: 1.5rem;
-  color: gray;
-`;
-
 const Header = ({ title, subtitle }) => {
   return (
-    <HeaderWrapper>
+    <div className="bg-dark h-screen flex justify-center items-center text-center">
       <div className="container">
-        <Title>{title}</Title>
-        <Subtitle>{subtitle}</Subtitle>
+        <h1 className="text-white text-3xl">{title}</h1>
+        <p className="text-gray-400 text-xl">{subtitle}</p>
       </div>
-    </HeaderWrapper>
+    </div>
   );
 };
 
